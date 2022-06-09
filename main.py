e6c1ebf5aa8b4bf810d3c0ba342fd15b9e0d9cb1
-IPC1-P2_Front_201709035 <rlopez2014ig@gmail.com>
2022-06-08 19:54:54 -0600

    config

---
+++ main.py
@@ -0,0 +1,15 @@
+from listaCircular_dobleEnlazada import ListaCircularDoble
+
+lista = ListaCircularDoble()
+
+print('-- Lista Completa: --')
+lista.agregar_final(12)
+lista.agregar_final(10)
+lista.agregar_final(8)
+lista.agregar_final(58)
+
+lista.recorrer_inicio_fin()
+print('---------------------')
+
+numero = int(input("Seleccione número: "))
+
