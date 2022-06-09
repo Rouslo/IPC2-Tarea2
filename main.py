bfe03160a13652d4d72e0ec8a7626718b19888f5
-IPC1-P2_Front_201709035 <rlopez2014ig@gmail.com>
2022-06-08 20:47:57 -0600

    config

--- main.py
+++ main.py
@@ -1,4 +1,51 @@
-from listaCircular_dobleEnlazada import ListaCircularDoble
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior = None
+
+class ListaCircularDoble:
+    def __init__(self) :
+        self.primero = None
+        self.ultimo = None
+
+    def vacia(self):
+        if self.primero == None:
+            return True
+        else:
+            return False
+
+    def agregar_inicio(self, dato):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(dato)
+        else:
+            aux = Nodo(dato)
+            aux.siguiente = self.primero
+            self.primero.anterior = aux
+            self.primero = aux
+        self.__unir_nodos()
+
+    def agregar_final(self, dato):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(dato)
+        else:
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = Nodo(dato)
+            self.ultimo.anterior = aux
+        self.__unir_nodos
+
+    def __unir_nodos(self):
+        self.primero.anterior = self.ultimo
+        self.ultimo.siguiente = self.primero
+
+    def recorrer_inicio_fin(self):
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux = aux.siguiente
+            if aux == self.primero:
+                break
+            
 
 lista = ListaCircularDoble()
 
